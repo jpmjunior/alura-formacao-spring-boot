7b056f7a82411f864ee7a8033e9d3decb16a5e20
jpmjunior <junior.linhares@yahoo.com.br>
2024-04-07 14:40:37 -0300

    entidade usuario e migration criadas

---
+++ src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    
+
+
+}
